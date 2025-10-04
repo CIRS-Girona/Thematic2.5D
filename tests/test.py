49231a450843a3b019e71c3a6430dcf6cb317767
Taqi Hamoda <taqihisham@gmail.com>
2025-10-04 18:40:57 +0200

    Finished implementing the testing script. Will enable github actions

--- tests/test.py
+++ tests/test.py
@@ -5,6 +5,9 @@ SAMPLES = ('18mm', '24mm', 'GPS')
 
 TEST_DIR = "tests"
 SAMPLE_DIR = f"{TEST_DIR}/samples"
+DATASET_DIR = f"{TEST_DIR}/data/dataset"
+FEATURES_DIR = f"{TEST_DIR}/data/features"
+MODELS_DIR = f"{TEST_DIR}/data/models"
 RESULTS_DIR = f"{TEST_DIR}/data/results"
 
 
@@ -27,40 +30,58 @@ def data_check(data_path, check_dir = False, check_empty=False):
         assert len(os.listdir(data_path)) > 0
 
 
-
 if __name__ == "__main__":
     with open("config.yaml", 'r') as f:
         config = yaml.safe_load(f)
 
-    config['train_model']['use_saved_features'] = False
+    # Create dataset from samples
     config['create_dataset']['enabled'] = True
+    config['train_model']['enabled'] = True
+    config['run_inference']['enabled'] = False
+    config['evaluate_results']['enabled'] = False
+
+    config['train_model']['dimension'] = '25'
+    config['train_model']['use_saved_features'] = False
 
     run_pipeline(config)
 
-    config['train_model']['use_saved_features'] = True
+    data_check(f"{DATASET_DIR}/2D", check_dir=True, check_empty=True)
+    data_check(f"{DATASET_DIR}/3D", check_dir=True, check_empty=True)
+    data_check(FEATURES_DIR, check_dir=True, check_empty=True)
+    data_check(MODELS_DIR, check_dir=True, check_empty=True)
+    data_check(RESULTS_DIR, check_dir=True)
+
+    # Train the different models on the dataset created
     config['create_dataset']['enabled'] = False
+    config['train_model']['use_saved_features'] = True
+    for model in MODELS:
+        for binary in (True, False):
+            config['train_model']['dimension'] = model
+            config['train_model']['binary_mode'] = binary
 
-    assert os.path.exists(RESULTS_DIR) and os.path.isdir(RESULTS_DIR)
+            run_pipeline(config)
 
+            file_name = f"SVM{model}_binary" if binary else f"SVM{model}"
+
+            data_check(f"{RESULTS_DIR}/{file_name}.jpg")
+            data_check(f"{RESULTS_DIR}/{file_name}.txt")
+
+    # Run the trained models on the samples
+    config['train_model']['enabled'] = False
     for sample in SAMPLES:
-        for model in MODELS:
-            for binary in (True, False):
-                config['train_model']['binary_mode'] = binary
-                config['train_model']['dimension'] = model
+        config['run_inference']['enabled'] = True
+        config['run_inference']['image_path'] = f"{SAMPLE_DIR}/{sample}/images"
+        config['run_inference']['depth_path'] = f"{SAMPLE_DIR}/{sample}/depths"
 
-                config['run_inference']['image_path'] = f"{SAMPLE_DIR}/{sample}/images"
-                config['run_inference']['depth_path'] = f"{SAMPLE_DIR}/{sample}/depths"
+        config['evaluate_results']['enabled'] = True
+        config['evaluate_results']['mask_path'] = f"{SAMPLE_DIR}/{sample}/masks"
 
-                config['evaluate_results']['mask_path'] = f"{SAMPLE_DIR}/{sample}/masks"
+        run_pipeline(config)
 
-                run_pipeline(config)
+    # Check data integrity
+    for model in MODELS:
+        for binary in (True, False):
+            file_name = f"SVM{model}_binary" if binary else f"SVM{model}"
 
-                data_check(
-                    f"{RESULTS_DIR}/SVM{model}",
-                    check_dir=True,
-                    check_empty=True
-                )
-
-                data_check(f"{RESULTS_DIR}/SVM{model}-mIoU.txt")
-                data_check(f"{RESULTS_DIR}/SVM{model}.jpg")
-                data_check(f"{RESULTS_DIR}/SVM{model}.txt")
+            data_check(f"{RESULTS_DIR}/{file_name}-mIoU.txt")
+            data_check(f"{RESULTS_DIR}/{file_name}", check_dir=True, check_empty=True)
